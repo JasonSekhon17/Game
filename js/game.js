232addc6b819efb3e106c1bc7e398a2c2ff5cdc9
Jason Sekhon <jason_2s@hotmail.com>
2017-03-30 12:46:43 -0700

    movement started

--- js/game.js
+++ js/game.js
@@ -6,47 +6,102 @@ window.addEventListener("load",function(){
 	})();
 
 	var canvas = document.getElementById("mycanvas")
-	ctx = canvas.getContext("2d")
-	canvas.width=384
-	canvas.height=216
-	player = {
-		x : canvas.width/2,
-		y : canvas.height/2,
+	var ctx = canvas.getContext("2d")
+    var width=600
+    var height=500
+	canvas.width=width
+	canvas.height=height
+	var player = {
+		x : width/2,
+		y : height/2,
 		width : 10,
 		height : 10,
-		speed : 1,
-		thrust : 0,
-		rot: 0
+		speed : 10,
+		thrustX : 0,
+        thrustY : 0,
+        distanceX : .1,
+        distanceY : 1,
+		rot : 0
 	}
-	bullet = {
-		x : canvas.width/2,
-		y : canvas.height-5,
-		width : 10,
-		height : 10,
-		speed : 6,
-		velX : 0,
-		velY : 0,
-	}
-	keys = []
-	friction = .99
-	gravity = 0.2
+	var keys = []
+	var friction = .99
+
+    function drawShip(){
+        ctx.fillStyle = "red"
+        ctx.beginPath()
+        ctx.moveTo(-2,2)
+        ctx.lineTo(2,10)
+        ctx.lineTo(-2,18)
+        ctx.lineTo(-25,10)
+        ctx.lineTo(-2,2)
+        ctx.strokeStyle = "red"
+        ctx.stroke()
+        ctx.fill()
+    }
+
+    function moveShip(){
+        if (keys[38]){
+            player.thrustX += player.distanceX * Math.cos(convertToRadians(player.rot));
+            player.thrustY += player.distanceY * Math.sin(convertToRadians(player.rot));
+            if(player.thrustX > player.speed){
+                player.thrustX = player.speed;
+            }
+            if(player.thrustY > player.speed){
+                player.thrustY = player.speed;
+            }
+        }
+        if (keys[39]){
+            incrementAngle()
+        }
+        if (keys[37]){
+            decrementAngle()
+        }
+        if (keys[32]){
+
+        }
+    }
+
+    function outOfBorders(){
+        if(player.x > width){
+            player.x = player.x - width
+        }
+        if(player.x< 0){
+            player.x = width
+        }
+        if(player.y > height){
+            player.y = player.y - height
+        }
+        if(player.y < 0){
+            player.y = height
+        }
+    }
+
+    function convertToRadians(degree) {
+        return degree*(Math.PI/180)
+    }
+
+    function incrementAngle() {
+        player.rot += 5
+        if(player.rot > 360){
+            player.rot = 0
+        }
+    }
+
+    function decrementAngle(){
+        player.rot -= 5
+        if(player.rot > 360){
+            player.rot = 0
+        }
+    }
 
 	function update(){
-		if (keys[38]){
-			if (player.thrust < player.speed){
-				player.thrust++
-			}
-		}
-		if (keys[39]){
-			rot++
-		}
-		if (keys[37]){
-			rot--
-		}
-		if (keys[32]){
-			
-		}
-		player.thrust *= friction
+        moveShip()
+        player.x += player.thrustX * player.distanceY
+        player.y += player.thrustY * player.distanceY
+        player.thrustX *= friction
+        player.thrustY *= friction
+        outOfBorders()
+		/*player.thrust *= friction
 		player.x += player.thrust
 		if (player.x >= canvas.width-player.width){
 			player.x = canvas.width-player.width
@@ -58,15 +113,12 @@ window.addEventListener("load",function(){
 		} else if(player.y <= 0){
 			player.y = 0
 		}
-		/*ctx.clearRect(0,0,canvas.width, canvas.height);
+		ctx.clearRect(0,0,canvas.width, canvas.height);
 		ctx.fillStyle = "red"
 		ctx.fillRect(player.x, player.y, player.width, player.height)
 		*/
-		ctx.beginPath()
-		ctx.moveTo(10, 10)
-		ctx.lineTo(10, 20)
-		ctx.lineTo(20, 20)
-		ctx.closePath
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        drawShip()
 		requestAnimationFrame(update)
 	}
 
